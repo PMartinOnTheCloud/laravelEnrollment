2ba4421342294232f85a0adfe31b86695293acce
kokodemo <xnominatorxx@gmail.com>
2021-03-09 21:01:38 +0100

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('terms', [TermController::class, 'all']);
+//Route::resource('terms', [TermController::class, 'all']);
