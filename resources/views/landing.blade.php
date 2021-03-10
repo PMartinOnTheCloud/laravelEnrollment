8806fed857ce0cb8860b6e416be8931697fa2aae
PMartinOnTheCloud <57746917+PMartinOnTheCloud@users.noreply.github.com>
2021-03-10 12:02:21 +0100

    Update landing.blade.php

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -3,11 +3,8 @@
 		<title>LEP</title>
 		<meta charset="utf-8">
 		<link rel="stylesheet" href="{{ asset('css/landing.css') }}">
-		<link rel="stylesheet" href="{{ asset('css/main.css') }}">
   	    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	    <script src="https://kit.fontawesome.com/74ec47558a.js" crossorigin="anonymous"></script>
-	    <link rel="stylesheet" type="text/css" href="../../public/css/landing.css"/>
-	    <link rel="stylesheet" type="text/css" href="../../public/css/main.css"/>
 	</head>
 	<body id="landing">
 		<div id="main_landing">
